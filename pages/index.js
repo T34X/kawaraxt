d8ed8cbf09ca82f7203d5f61f9b7b6f234873cdb
user6879 <akumaru.senju@gmail.com>
2023-03-25 06:37:42 +0500

    working with components and global styles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
-export default function HomePage(){
-  return <>
-    <h1 className="text-3xl text-indigo-800">Hello World!</h1>
-  </>
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>I'm Triistam</p>
+        <p>
+          17y.o. coder from Uzbekistan. I'm currently learning typescript for
+          web development!
+        </p>
+      </section>
+    </Layout>
+  );
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,20 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "@/components/layout";
+
 export default function FirstPost() {
-    return <h1>My First Post</h1>;
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>First post</title>
+        </Head>
+        <h1>First Post</h1>
+
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </Layout>
+    </>
+  );
 }
